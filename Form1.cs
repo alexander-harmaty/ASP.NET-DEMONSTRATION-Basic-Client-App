e71a2a6a72207d9ed7ebb92899e6cc26b091764f
alexander-harmaty <92049896+alexander-harmaty@users.noreply.github.com>
2022-05-13 18:36:22 -0400

    update
    "Users" table name issue

--- Form1.cs
+++ Form1.cs
@@ -8,23 +8,40 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using Newtonsoft.Json;
+using System.Net.Http;
+using System.Net.Http.Formatting;
 
 namespace MyClientApp
 {
     public partial class Form1 : Form
     {
         localhost.WebService1 proxy = new localhost.WebService1();
+        HttpClient client = new HttpClient();
+
         public Form1()
         {
             InitializeComponent();
         }
 
+        private void WebServicesSettings()
+        {
+            client.BaseAddress = new Uri("https://localhost:44335/WebService1.asmx/");
+        }
+
         private void Form1_Load(object sender, EventArgs e)
         {
-            string countriesJson = proxy.Countries();
-            DataTable dtCountries = JsonConvert.DeserializeObject<DataTable>(countriesJson);
+            //string countriesJson = proxy.Countries();
+            //DataTable dtCountries = JsonConvert.DeserializeObject<DataTable>(countriesJson);
+            //dataGridView1.DataSource = dtCountries;
 
-            dataGridView1.DataSource = dtCountries;
+            WebServicesSettings();
+        }
+
+        private void btnSearch_Click(object sender, EventArgs e)
+        {
+            HttpResponseMessage message = client.GetAsync("dataTableForUsers?id=" + textBoxID.Text + "").Result;
+            string userJson = message.Content.ReadAsStringAsync().Result;
+            MessageBox.Show(userJson);
         }
     }
 }
