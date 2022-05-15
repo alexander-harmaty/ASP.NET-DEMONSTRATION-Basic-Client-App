f1b11c79a4e74bf2ef2867f38a84da2eb1c8489d
alexander-harmaty <92049896+alexander-harmaty@users.noreply.github.com>
2022-05-14 21:19:05 -0400

    revert

--- Form1.cs
+++ Form1.cs
@@ -16,43 +16,43 @@ namespace MyClientApp
     public partial class Form1 : Form
     {
         localhost.WebService1 proxy = new localhost.WebService1();
-        HttpClient client = new HttpClient();
+        //HttpClient client = new HttpClient();
 
         public Form1()
         {
             InitializeComponent();
         }
 
-        private void WebServicesSettings()
-        {
-            client.BaseAddress = new Uri("https://localhost:44335/WebService1.asmx/");
-        }
+        //private void WebServicesSettings()
+        //{
+        //    client.BaseAddress = new Uri("https://localhost:44335/WebService1.asmx/");
+        //}
 
         private void Form1_Load(object sender, EventArgs e)
         {
-            //string countriesJson = proxy.Countries();
-            //DataTable dtCountries = JsonConvert.DeserializeObject<DataTable>(countriesJson);
-            //dataGridView1.DataSource = dtCountries;
+            string countriesJson = proxy.Countries();
+            DataTable dtCountries = JsonConvert.DeserializeObject<DataTable>(countriesJson);
+            dataGridView1.DataSource = dtCountries;
 
-            WebServicesSettings();
+            //WebServicesSettings();
         }
 
-        private DataTable stringSplit(string userJson)
-        {
-            string[] json = userJson.Split('>');
-            string[] finalJson = json[2].Split('<');
+        //private DataTable stringSplit(string userJson)
+        //{
+        //    string[] json = userJson.Split('>');
+        //    string[] finalJson = json[2].Split('<');
 
-            DataTable dt = JsonConvert.DeserializeObject<DataTable>(finalJson[0]);
-            return dt;
-        }
+        //    DataTable dt = JsonConvert.DeserializeObject<DataTable>(finalJson[0]);
+        //    return dt;
+        //}
 
         private void btnSearch_Click(object sender, EventArgs e)
         {
-            HttpResponseMessage message = client.GetAsync("dataTableForUsers?id=" + textBoxID.Text + "").Result;
-            string userJson = message.Content.ReadAsStringAsync().Result;
-            //MessageBox.Show(userJson);
+            //HttpResponseMessage message = client.GetAsync("dataTableForUsers?id=" + textBoxID.Text + "").Result;
+            //string userJson = message.Content.ReadAsStringAsync().Result;
+            ////MessageBox.Show(userJson);
 
-            dataGridView1.DataSource = stringSplit(userJson);
+            //dataGridView1.DataSource = stringSplit(userJson);
         }
     }
 }
